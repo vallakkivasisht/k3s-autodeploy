8585fc72717257df95b29f4a9eea4f36438e5a3c
Mounika3001 <mounikam0471>
2025-07-28 11:34:01 +0530

    Updated app.py with new logic

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on K3s!"
+    return "Vallakki says hello"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
